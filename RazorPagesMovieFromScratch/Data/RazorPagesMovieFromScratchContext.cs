dee1913da669b50da2f1b9505041be4b801dcf86
radim cimoradsky <radim.cimoradsky@outlook.com>
2020-02-11 17:11:55 -0500

    RazorPagesMovieFromScratch project

---
+++ RazorPagesMovieFromScratch/Data/RazorPagesMovieFromScratchContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovieFromScratch.Models;
+
+namespace RazorPagesMovieFromScratch.Data
+{
+    public class RazorPagesMovieFromScratchContext : DbContext
+    {
+        public RazorPagesMovieFromScratchContext (DbContextOptions<RazorPagesMovieFromScratchContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovieFromScratch.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ RazorPagesMovieFromScratch/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovieFromScratch.Data;
+using RazorPagesMovieFromScratch.Models;
+
+namespace RazorPagesMovieFromScratch
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovieFromScratch.Data.RazorPagesMovieFromScratchContext _context;
+
+        public IndexModel(RazorPagesMovieFromScratch.Data.RazorPagesMovieFromScratchContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
