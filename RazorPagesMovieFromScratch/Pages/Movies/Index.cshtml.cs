194f12c74dd97394ba259dee551547699313878e
Radim <radimx@gmail.com>
2020-04-21 00:01:31 +0200

    Completed Add Search feature

--- RazorPagesMovieFromScratch/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovieFromScratch/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovieFromScratch.Data;
 using RazorPagesMovieFromScratch.Models;
@@ -19,11 +20,40 @@ namespace RazorPagesMovieFromScratch
             _context = context;
         }
 
-        public IList<Movie> Movie { get;set; }
+        public IList<Movie> Movie { get; set; }
+        
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        
+        
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Genres { get; set; }
+        
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            // Use LINQ to get list of movies.
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+            //Movie = await _context.Movie.ToListAsync();
+            Movie = await movies.ToListAsync();
+
+
+            // Use LINQ to get list of genres.
+            IQueryable<string> allGenres = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+            Genres = new SelectList(await allGenres.Distinct().ToListAsync());
         }
     }
 }
