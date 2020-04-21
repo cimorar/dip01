6a99466faa561bd20e1f4f5807ac95d709bcc14c
Radim <radimx@gmail.com>
2020-04-21 23:53:53 +0200

    Completed: Add a new field

--- RazorPagesMovieFromScratch/Migrations/20200421214308_Add_Movie_Rating.Designer.cs
+++ RazorPagesMovieFromScratch/Migrations/20200421214308_Add_Movie_Rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovieFromScratch.Data;
 
 namespace RazorPagesMovieFromScratch.Migrations
 {
     [DbContext(typeof(RazorPagesMovieFromScratchContext))]
-    partial class RazorPagesMovieFromScratchContextModelSnapshot : ModelSnapshot
+    [Migration("20200421214308_Add_Movie_Rating")]
+    partial class Add_Movie_Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
