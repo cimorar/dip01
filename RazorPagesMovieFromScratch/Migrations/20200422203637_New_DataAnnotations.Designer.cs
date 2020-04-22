694007472bdc567f775cc42e0ec9942ff7973a70
Radim <radimx@gmail.com>
2020-04-22 22:38:53 +0200

    final changes in Add Validation chapter

--- RazorPagesMovieFromScratch/Migrations/20200422203637_New_DataAnnotations.Designer.cs
+++ RazorPagesMovieFromScratch/Migrations/20200422203637_New_DataAnnotations.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovieFromScratch.Data;
 
 namespace RazorPagesMovieFromScratch.Migrations
 {
     [DbContext(typeof(RazorPagesMovieFromScratchContext))]
-    partial class RazorPagesMovieFromScratchContextModelSnapshot : ModelSnapshot
+    [Migration("20200422203637_New_DataAnnotations")]
+    partial class New_DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
